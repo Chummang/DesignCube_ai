daf59cfe6998c9bab203443fabdb0782e63e9454
Chummang <112371987+Chummang@users.noreply.github.com>
2024-03-05 17:25:39 +0800

    Update index.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -14,7 +14,7 @@ const CanvasModel = () => {
     className="w-full max-w-full h-full transition-all ease-in"
     > 
       <ambientLight intensity={0.5}/>
-      <Environment files="/public/potsdamer_platz_1k.hdr" />
+      <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
 
       <CameraRig>
         <Backdrop />
